c1a0ba39a77a11428f201fc135b2bc118ff3db83
X. Michalet <michalet@chem.ucla.edu>
2021-11-08 17:45:34 -0800

    updated conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,10 +29,10 @@ copyright = '2021, Regents of the University of California'
 author = 'X. Michalet'
 
 # The short X.Y version.
-version = '0.3'
+version = '0.4.1'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3'
+release = '0.4.1.3'
 
 
 # -- General configuration ---------------------------------------------------
